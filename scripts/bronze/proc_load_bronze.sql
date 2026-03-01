d46437e07206c8ff348437f8193db78d7cf8d3dd
Bunny1026-cs <140492628+Bunny1026-cs@users.noreply.github.com>
2026-03-01 15:18:33 +0530

    Refactor load_bronze procedure for clarity and structure
    Refactored the procedure to include BEGIN and END blocks, ensuring proper structure. The BULK INSERT statements are now grouped under their respective TRUNCATE TABLE statements for clarity.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,68 +1,72 @@
--- CRM TABLES
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 
-TRUNCATE TABLE bronze.crm_cust_info;
+BEGIN
 
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+    TRUNCATE TABLE bronze.crm_cust_info;
 
-
-TRUNCATE TABLE bronze.crm_prd_info;
-
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+    BULK INSERT bronze.crm_cust_info
+    FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
 
 
-TRUNCATE TABLE bronze.crm_sales_details;
+    TRUNCATE TABLE bronze.crm_prd_info;
 
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-
-
--- ERP TABLES
-
-TRUNCATE TABLE bronze.erp_loc_a101;
-
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+    BULK INSERT bronze.crm_prd_info
+    FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
 
 
-TRUNCATE TABLE bronze.erp_cust_az12;
+    TRUNCATE TABLE bronze.crm_sales_details;
 
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+    BULK INSERT bronze.crm_sales_details
+    FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
 
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+
+    TRUNCATE TABLE bronze.erp_loc_a101;
+
+    BULK INSERT bronze.erp_loc_a101
+    FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
+
+
+    TRUNCATE TABLE bronze.erp_cust_az12;
+
+    BULK INSERT bronze.erp_cust_az12
+    FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
+
+
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+    BULK INSERT bronze.erp_px_cat_g1v2
+    FROM 'C:\Users\dell\Downloads\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
+
+END
